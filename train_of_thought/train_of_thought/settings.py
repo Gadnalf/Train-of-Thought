b1d8ffd9a44e7a4ac1761279f67781f1e1ab1e3d
Philip Cai <32752007+Gadnalf@users.noreply.github.com>
2020-01-16 17:16:55 -0500

    Update settings.py

--- train_of_thought/train_of_thought/settings.py
+++ train_of_thought/train_of_thought/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,4 +119,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+if(!debug):
+    import django_heroku
+    django_heroku.settings(locals())
